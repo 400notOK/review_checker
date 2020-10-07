cd73ef2f7b6ebdb0b925aeaf2b204e5d9176d926
400notOK <50711155+400notOK@users.noreply.github.com>
2020-10-07 13:16:21 +0300

    Fixes, add requirements and readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# review_checker
+# Review checker for dvmn tasks
+
+Telegram bot for sending notifications about verification of works on [dvmn.org](https://dvmn.org/modules).
+### How to install
+
+Python3 should be already installed. Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+Create .env file with environment variables:
+```
+DEVMAN_API_TOKEN=<DEVMAN_API_TOKEN>
+TELEGRAM_TOKEN=<TELEGRAM_TOKEN>
+TG_CHAT_ID=<TELEGRAM_CHAT_ID>
+```
+
+### Usage
+```
+python main.py
+```
+
+### Project Goals
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org)
  
--- dvmn_api.py
+++ dvmn_api.py
@@ -1,39 +1,69 @@
 import requests
-import json
-import telegram
 import os
+import time
+import telegram
 from urllib.parse import urljoin
 from dotenv import load_dotenv
 load_dotenv()
 
-bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
-TOKEN = dict(Authorization=f'Token {os.getenv("DEVMAN_API_TOKEN")}')
-URL = 'https://dvmn.org/api/long_polling/'
-CHAT_ID = os.getenv('CHAT_ID')
-timestamp = None
 
-while True:
-    try:
-        query = requests.get(URL, headers=TOKEN, params=dict(timestamp=timestamp)).json()
-        query_output = json.dumps(query, ensure_ascii=False, sort_keys=True, indent=4)
-        print(query_output)
-        timestamp = query.get('timestamp_to_request')
-        if query['status'] == 'found':
-            bot.send_message(
-                chat_id=CHAT_ID,
-                text=f'Преподаватель проверил работу! \n'
-                     f'«{query["new_attempts"][0]["lesson_title"]}»\n'
-                     f'Ссылка на урок: {urljoin("https://dvmn.org/", query["new_attempts"][0]["lesson_url"])}')
+def main():
+    bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
+    TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+    URL_DVM_QUERY = 'https://dvmn.org/api/long_polling/'
+    timestamp = time.time()
+    while True:
+        try:
+            query_to_dvm_api = requests.get(
+                URL_DVM_QUERY,
+                headers={'Authorization': f'Token {os.getenv("DEVMAN_API_TOKEN")}'},
+                params={'timestamp': timestamp}
+            ).json()
+            query_to_dvm_api.raise_for_status()
+            if 'error' in query_to_dvm_api.json():
+                raise requests.exceptions.HTTPError(query_to_dvm_api['error'])
+            timestamp = query_to_dvm_api.get('timestamp_to_request')
 
-            if query["new_attempts"][0]["is_negative"] is True:
-                bot.send_message(chat_id=CHAT_ID, text='Нужно внести правки по ревью!')
-            else:
-                bot.send_message(chat_id=CHAT_ID, text='Код идеален, можно делать следующий урок!')
+            status = query_to_dvm_api.get('status')
 
-            timestamp = query.get('last_attempt_timestamp')
-        if query['status'] == 'timeout':
-            timestamp = query.get('timestamp_to_request')
-    except requests.exceptions.ReadTimeout:
-        pass
-    except ConnectionError:
-        pass
+            if status == 'found':
+                new_attempts = query_to_dvm_api['new_attempts'][0]
+                lesson = new_attempts['lesson_title']
+                is_negative = new_attempts['is_negative']
+                timestamp = query_to_dvm_api['last_attempt_timestamp']
+                lesson_url = query_to_dvm_api["new_attempts"][0]["lesson_url"]
+
+                if is_negative:
+                    bot.send_message(
+                        chat_id=TG_CHAT_ID,
+                        text=f'''
+                         Преподаватель проверил работу!
+                         «{lesson}»
+                         Ссылка на урок: {urljoin('https://dvmn.org/', lesson_url)}
+                         
+                         Нужно внести правки по ревью!
+                    ''')
+                else:
+                    bot.send_message(
+                        chat_id=TG_CHAT_ID,
+                        text='''
+                        Преподаватель проверил работу!
+                        «{lesson}»
+                        Ссылка на урок: {urljoin('https://dvmn.org/', lesson_url)}
+                        
+                        Нужно внести правки по ревью!
+                        ''')
+                    bot.send_message(chat_id=TG_CHAT_ID, text='Код идеален, можно делать следующий урок!')
+
+                timestamp = query_to_dvm_api.get('last_attempt_timestamp')
+            if status == 'timeout':
+                timestamp = query_to_dvm_api.get('timestamp_to_request')
+        except requests.exceptions.ReadTimeout:
+            pass
+        except ConnectionError:
+            time.sleep(5)
+            pass
+
+
+if __name__ == '__main__':
+    main()
